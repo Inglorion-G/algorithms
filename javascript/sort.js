06cd65e3e3bb5641946cf029b625fe0e0a960b4a
George Groh <george.groh@gmail.com>
2016-02-15 18:20:16 -0800

    Add a set up function for sort tests, add merge sort and insertion sort

--- javascript/sort.js
+++ javascript/sort.js
@@ -8,7 +8,7 @@
         O(n^2)
 */
 
-function selectionSort(arr) {
+exports.selectionSort = function (arr) {
     var n = arr.length;
 
     for (var i = 0; i < n; i++) {
@@ -28,6 +28,88 @@ function selectionSort(arr) {
     return arr;
 }
 
-module.exports = {
-    selectionSort: selectionSort
-};
+/*
+    Insertion Sort
+    Inputs --
+        an Array
+    Outputs --
+        the input Array, sorted
+    Complexity --
+        Average complexity is O(n^2), however in the worst case insertion sort
+        will move elements n^2 times, whereas selection sort will move
+        elements only n times
+
+*/
+
+exports.insertionSort = function (arr) {
+    var n = arr.length;
+
+    for (i = 1; i < n; i++) {
+        var key = arr[i],
+            j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+
+/*
+    Merge Sort
+    Inputs --
+        an Array, arr
+        starting index, p
+        ending index, r
+    Outputs --
+        a sorted subarray arr[p..r]
+    Complexity --
+        Time: O(n log n)
+        Space: Each element is copied O(n log n) times
+*/
+
+function merge(left, right) {
+    var merged = [],
+        i      = 0,
+        j      = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] > right[j]) {
+            merged.push(right[j]);
+            j += 1;
+        } else {
+            merged.push(left[i]);
+            i += 1;
+        }
+    }
+
+    var leftovers = left.slice(i, left.length).concat(right.slice(j, right.length));
+    merged = merged.concat(leftovers);
+
+    return merged;
+}
+
+var mergeSort = exports.mergeSort = function (arr) {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        var mid   = Math.floor((arr.length) / 2),
+            left  = arr.slice(0, mid),
+            right = arr.slice(mid, arr.length);
+
+        left  = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+}
+
+
+
+
+
+
--- javascript/test/sort_tests.js
+++ javascript/test/sort_tests.js
@@ -1,21 +1,34 @@
 var sort = require('../sort');
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+exports.setUp = function (callback) {
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    this.testArray   = [];
+    this.sortedArray = [];
+
+    for (var i = 0; i < 1000; i++) {
+        var randInt = getRandomInt(1, 500);
+        this.testArray.push(randInt);
+        this.sortedArray.push(randInt);
+    }
+
+    this.sortedArray.sort((a, b) => a - b);
+    callback();
 }
-
-var testArray   = [],
-    sortedArray = [];
-
-for (var i = 0; i < 10; i++) {
-    var randInt = getRandomInt(1, 500);
-    testArray.push(randInt);
-    sortedArray.push(randInt);
-}
-
-sortedArray.sort((a, b) => a - b);
 
 exports.selectionSort = function (test) {
-    test.deepEqual(sort.selectionSort(testArray), sortedArray);
+    test.deepEqual(sort.selectionSort(this.testArray), this.sortedArray);
+    test.done();
+}
+
+exports.insertionSort = function (test) {
+    test.deepEqual(sort.insertionSort(this.testArray), this.sortedArray);
+    test.done();
+}
+
+exports.mergeSort = function (test) {
+    test.deepEqual(sort.mergeSort(this.testArray), this.sortedArray);
     test.done();
 }
